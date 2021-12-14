60bd68d084cd21e5e2f3f3776da66207f44462e7
rambunctiousapple <75225425+rambunctiousapple@users.noreply.github.com>
2021-12-13 21:54:57 -0800

    added TODO to solve registrator problems.

--- deps/build.jl
+++ deps/build.jl
@@ -2,6 +2,8 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # See LICENSE in the project root for full license information.
 
+#TODO move this code into generate.jl. Change AGFFileReader.jl so it doesn't install glass files automatically. It looks like
+#generating files on import is frowned upon by Julia Registrator. Make user do it manually.
 include(joinpath("..", "src", "constants.jl"))
 include(joinpath(GLASSCAT_DIR, "GlassTypes.jl"))
 include(joinpath(GLASSCAT_DIR, "sources.jl"))
